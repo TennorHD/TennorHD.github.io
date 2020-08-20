02ac0dc65ed75d54a292f8f7acecec02ed4c3c6e
TennorHD <hcykl2@gmail.com>
2020-08-20 20:03:07 +0800

    change a bit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 
 var home = new Vue({
-    el: 'home',
+    el: '#home',
     data: {
-        profile_picture: 'this is profile picture',
+        message: 'this is profile picture',
     }
 })
