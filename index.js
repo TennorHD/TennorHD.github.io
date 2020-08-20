60b90136c92e285c9ff90d5ff213cdfeac0fda9d
TennorHD <hcykl2@gmail.com>
2020-08-21 03:39:03 +0800

    built first page

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-import Vue from 'vue'
-
 var home = new Vue({
-    el: '#home',
+    el: '.home',
     data: {
-        message: 'this is profile picture',
+        prof_pic: './img/Profile.png',
+        page_title: 'Hello there!',
+        span_1: '< ',
+        span_2: '/>',
+        subtitle: 'This is a Tennor Lee\'s space, connect with me to know more!',
+        logos: [
+            {id:1,img:'./img/github-logo.png',url:'https://github.com/TennorHD'},
+            {id:2,img:'./img/instagram-logo.png',url:'https://www.instagram.com/hi.im_ning/'},
+            {id:3,img:'./img/twitter-logo.png',url:'https://twitter.com/tennorlee'}
+        ]
+    }
+})
+
+var nav_bar = new Vue({
+    el: '.navigation-container',
+    data: {
+        title: 'Tennor Lee',
+        nav_items: [
+            {id:1,value:'About'},
+            {id:2,value:'Blog'},
+            {id:3,value:'Contact'},
+        ]
     }
 })
