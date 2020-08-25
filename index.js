d1e74b75da4cbfb4e3d03ef3412a2362d71f6c31
TennorHD <hcykl2@gmail.com>
2020-08-25 22:39:29 +0800

    added responsive design and design on desktop view

--- index.js
+++ index.js
@@ -10,19 +10,31 @@ var home = new Vue({
             {id:1,img:'./img/github-logo.png',url:'https://github.com/TennorHD'},
             {id:2,img:'./img/instagram-logo.png',url:'https://www.instagram.com/hi.im_ning/'},
             {id:3,img:'./img/twitter-logo.png',url:'https://twitter.com/tennorlee'}
-        ]
+        ],
+        wave: './img/landingpage_wave.svg'
     }
 })
+
+let items = [
+    {id:1,value:'About',classname:'about'},
+    {id:2,value:'Projects',classname:'projects'},
+    {id:3,value:'Blog',classname:'blog'},
+    {id:4,value:'Contact',classname:'contact'},
+]
 
 var nav_bar = new Vue({
     el: '.navigation-container',
     data: {
-        title: 'Tennor Lee',
-        nav_items: [
-            {id:1,value:'About',classname:'about'},
-            {id:2,value:'Projects',classname:'projects'},
-            {id:3,value:'Blog',classname:'blog'},
-            {id:4,value:'Contact',classname:'contact'},
-        ]
+        title: 'tennorlee',
+        nav_items: items,
     }
+})
+
+var container = new Vue({
+    el: '.container',
+    data: {
+        windowwidth: window.innerWidth,
+        header: '<tennorlee/>',
+        items: items,
+    },
 })
